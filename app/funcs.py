c3f9cef6d436faae6c501537858a7fb941a1747f
SamirIngley <samir.ingle7@gmail.com>
2026-07-12 22:32:13 -0700

    codecrafters submit [skip ci]

--- app/funcs.py
+++ app/funcs.py
@@ -1,7 +1,19 @@
 import sys
 
-def exit_cmd():
+def exit_cmd(user_input):
+    if user_input != "exit":
+        return print("exit command does not accept arguments or trailing spaces")
     return "exit"
 
+def echo_cmd(user_input):
+    args_to_echo = user_input.split(' ',1)[1]
+    print(args_to_echo)
+    return 
 
-VALID_COMMANDS = {"exit":exit_cmd}
+
+VALID_COMMANDS = {
+    
+    "exit":exit_cmd,
+    "echo":echo_cmd,
+
+    }
--- app/main.py
+++ app/main.py
@@ -5,13 +5,15 @@ def main():
 
     while True:
         sys.stdout.write("$ ")
-        command = input()
+        user_input = input()
 
-        if command:
+        if user_input:
+            command = user_input.split()[0]
+            
             if command not in VALID_COMMANDS.keys():
                 print(f"{command}: command not found")
             else: 
-                output_flag = VALID_COMMANDS[command]()
+                output_flag = VALID_COMMANDS[command](user_input)
 
                 if output_flag == "exit": 
                     break
